34a906739ec98eb7df474ee4b4c540791d44c42c
Marius Holm <marho@itu.dk>
2024-08-28 21:32:01 +0200

    removed test file

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+try
+{
+    using StreamReader reader = new StreamReader("chirp_cli_db.csv");
+    string line;
+    reader.ReadLine();
+    while ((line = reader.ReadLine()) != null)
+    {
+        Console.WriteLine(line);
+        
+        
+        //string time = line.Substring(line.)
+        //Console.WriteLine(time);
+        
+       
+        Regex nameSeperator = new Regex(",\"");
+        string[] names = nameSeperator.Split(line);
+        
+        Console.WriteLine(names[0]);
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine("did not ");
+}
