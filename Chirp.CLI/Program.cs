9feaf60fef3b8219697ed795401ee81067963437
Pows <mariusholm@hotmail.dk>
2024-09-04 21:01:55 +0200

    Renamed variables to more fitting names

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -24,12 +24,12 @@ static void read()
     using (var reader = new StreamReader("chirp_cli_db.csv"))
     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
     {
-        var records = csv.GetRecords<Cheep>(); //We use the way we setup the Cheep class to "map" to how we stored the information in the csv file (it has a header - Author,Message,Timestamp)
-        foreach (var record in records)
+        var cheeps = csv.GetRecords<Cheep>(); //We use the way we setup the Cheep class to "map" to how we stored the information in the csv file (it has a header - Author,Message,Timestamp)
+        foreach (var cheep in cheeps)
         {
             DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dateTime = dateTime.AddSeconds(record.Timestamp).ToLocalTime();
-            Console.WriteLine(record.Author + " @ " + dateTime + ": " + record.Message);
+            dateTime = dateTime.AddSeconds(cheep.Timestamp).ToLocalTime();
+            Console.WriteLine(cheep.Author + " @ " + dateTime + ": " + cheep.Message);
         }
         
     }
@@ -47,8 +47,8 @@ static void cheep(string[] message)
     Cheep newCheep = new Cheep(userName, text, dateTime);
     
     //Store it in a record ready to write to csv file by using CsvHelper
-    var records = new List<Cheep>();
-    records.Add(newCheep);
+    var cheeps = new List<Cheep>();
+    cheeps.Add(newCheep);
     
     using (var writer = new StreamWriter("chirp_cli_db.csv"))
     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
@@ -56,7 +56,7 @@ static void cheep(string[] message)
         //TODO it deletes the current data in the csv file
         
         //csv.NextRecord(); 
-        csv.WriteRecords(records);
+        csv.WriteRecords(cheeps);
     }
     
     
