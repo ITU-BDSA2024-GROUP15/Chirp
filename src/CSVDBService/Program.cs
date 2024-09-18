f7cc4862043cf197892b01038893d698176a83d0
Marius Holm <marho@itu.dk>
2024-09-18 16:00:46 +0200

    Added functionality for reading cheeps with a limit
    Co-authored-by: Mads <mawc@itu.dk>
    Co-authored-by: Morten <mofb@itu.dk>
    Co-authored-by: Kassandra <kwad@itu.dk>
    Co-authored-by: Ida <iast@itu.dk>

--- src/CSVDBService/Program.cs
+++ src/CSVDBService/Program.cs
@@ -1,10 +1,22 @@
+using SimpleDB;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+CSVDatabase<Cheep> database = CSVDatabase<Cheep>.GetInstance();
+
 //Returns cheeps to the user
-app.MapGet("/cheeps", () => { ... });
+app.MapGet("/cheeps", (int? limit) =>
+{
+    return database.Read(limit);
+});
 
 //Add a cheep to the database
-app.MapPost("/cheep", (Cheep cheep) => { ... });
+app.MapPost("/cheep", (Cheep cheep) =>
+{
+    database.Store(cheep); 
+});
 
 app.Run();
+
+public record Cheep(string Author, string Message, long Timestamp);
