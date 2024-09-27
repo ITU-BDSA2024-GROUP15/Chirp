28bb1ce7d77e073f8875b6a214e81ad80a9f1895
Marius Holm <marho@itu.dk>
2024-09-27 15:17:02 +0200

    Added ORDER BY to query to print cheeps in correct order
    Co-authored-by: Mads <mawc@itu.dk>
    Co-authored-by: Morten <mofb@itu.dk>
    Co-authored-by: Kassandra <kwad@itu.dk>
    Co-authored-by: Ida <iast@itu.dk>

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -15,14 +15,14 @@ public class CheepService : ICheepService
     public List<Cheep> GetCheeps()
     {
         var facade = new DBFacade<Cheep>();
-        return facade.read(@"Select U.username, M.text, M.pub_date from message M join user U on M.author_id = U.user_id");
+        return facade.read(@"Select U.username, M.text, M.pub_date from message M join user U on M.author_id = U.user_id ORDER BY M.pub_date DESC");
     }
 
     public List<Cheep> GetCheepsFromAuthor(string author)
     {
         // filter by the provided author name
         var facade = new DBFacade<Cheep>();
-        return facade.read($"Select U.username, M.text, M.pub_date from message M join user U on M.author_id = U.user_id where U.username = \"{author}\""); 
+        return facade.read($"Select U.username, M.text, M.pub_date from message M join user U on M.author_id = U.user_id where U.username = \"{author}\" ORDER BY M.pub_date DESC"); 
     }
 
 }
