e0e582b664101a52d6e64e72bc82bef28f319d60
Mads Christensen <Mawc@itu.dk>
2024-09-27 13:40:00 +0200

    Added try-catch to DBFacade.cs
    Co-authored-by: Morten <mofb@itu.dk>
    Co-authored-by: Marius <marho@itu.dk>
    Co-authored-by: Kassandra <kwad@itu.dk>
    Co-authored-by: Ida <iast@itu.dk>

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -8,26 +8,40 @@ public class DBFacade<T>
     {
         string sqlDBFilePath = "./data/chirp.db";
         IEnumerable<Cheep> records = new List<Cheep>();
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
-        {
-            connection.Open();
+        
 
-            var command = connection.CreateCommand();
-            command.CommandText = query;
-            
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+        using ( var connection = new SqliteConnection($"Data Source={sqlDBFilePath}") )
+        {
+            try
             {
-                var cheep = new Cheep(
-                    Convert.ToString(reader["author_id"]),
-                    Convert.ToString(reader["text"]),
-                    Convert.ToInt64(reader["pub_date"]) // decimal will be better for money
-                );
-                records.Append(cheep);
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+
+                using var reader = command.ExecuteReader();
+                while ( reader.Read() )
+                {
+                    var cheep = new Cheep(
+                        Convert.ToString(reader["author_id"]),
+                        Convert.ToString(reader["text"]),
+                        Convert.ToInt64(reader["pub_date"])
+                    );
+                    records.Append(cheep);
+                }
             }
-            connection.Close();
+            catch ( SqliteException e )
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return records;
+
         }
-        return records;
+        
     }
     
 }
