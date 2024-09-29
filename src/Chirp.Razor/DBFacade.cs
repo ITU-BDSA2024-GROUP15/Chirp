e7d08cb026b74d71654813c980b2840c03c147e9
Kassandra <kwad@itu.dk>
2024-09-29 13:43:48 +0200

    Parsed unix timestamp to actual readable time and date in DBFacade.
    Co-authored-by: Morten <mofb@itu.dk>
    Co-authored-by: Marius <marho@itu.dk>
    Co-authored-by: Ida <iast@itu.dk>

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -17,7 +17,6 @@ public class DBFacade<T>
     public List<Cheep> read(string query)
     {
         
-        //string PATH = GetPathToDB();
         List<Cheep> records = new List<Cheep>();
         
 
@@ -36,7 +35,7 @@ public class DBFacade<T>
                     var cheep = new Cheep(
                         Convert.ToString(reader["username"]),
                         Convert.ToString(reader["text"]),
-                        Convert.ToInt64(reader["pub_date"])
+                        parseDateTime(Convert.ToInt64(reader["pub_date"]))
                     );
                     records.Add(cheep);
                 }
@@ -70,6 +69,29 @@ public class DBFacade<T>
         }
         pathToDB += "/src/Chirp.Razor/data/chirp.db";
         return pathToDB;
+        
+    }
+    
+    /// <summary>
+    /// This method converts unix time into the AM/PM format
+    /// </summary>
+    /// <param name="timestamp">the unix timestamp</param>
+    /// <returns>The converted time from the unix timestamp</returns>
+    public static string parseDateTime(long timestamp)
+    {
+        //Convert unix timestamp to datetime (Datetimeoffset)
+        DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeSeconds(timestamp);
+        
+        //We get the danish timezone
+        TimeZoneInfo danishTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+        
+        //Converts the timestamp to danish timezone
+        DateTime timeStamp = TimeZoneInfo.ConvertTime(dateTime, danishTimeZone).DateTime;
+        
+        //Formatting timestamp/date
+        string timeStampFormatted = timeStamp.ToString("HH:mm:ss dd-MM-yyyy");
+        return timeStampFormatted;
+        
         
     }
     
