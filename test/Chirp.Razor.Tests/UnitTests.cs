dc4643865dc3f690c61a462dd4d572cd8ff3cac8
KassandraW <kwad@itu.dk>
2024-10-01 13:55:48 +0200

    Added  TestParseDateTime test
    Co-authored-by: Mads <mawc@itu.dk>

--- test/Chirp.Razor.Tests/UnitTests.cs
+++ test/Chirp.Razor.Tests/UnitTests.cs
@@ -6,8 +6,14 @@ namespace Chirp.Razor.Tests;
 public class UnitTests
 {
     [Fact]
-    public void Test1()
+    public void testsomething()
     {
-
+        
+    }
+    
+    [Fact]
+    public void TestParseDateTime()
+    {
+        Assert.Equal("23:21:56 07-09-2024", DBFacade<Cheep>.parseDateTime(1725744116));
     }
 }
