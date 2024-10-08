166aef75478cbe334ab57261b3f96f8f29a33a3e
Marius Holm <marho@itu.dk>
2024-10-08 22:16:00 +0200

    Redeploying web app

--- src/Chirp.Razor/CheepDTO.cs
+++ src/Chirp.Razor/CheepDTO.cs
@@ -2,7 +2,7 @@
 
 public class CheepDTO
 {
-    public string message { get; set; }
+    public string message { get; set; } 
     public DateTime timestamp { get; set; }
     public string author { get; set; }
 }
