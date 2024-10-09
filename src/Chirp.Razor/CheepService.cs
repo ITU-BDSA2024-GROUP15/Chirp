45112085e7c4c41f7abc3b377c5806b4b919decf
Marius Holm <marho@itu.dk>
2024-10-09 13:47:07 +0200

    Fixed errors in CheepService.cs

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,6 +1,4 @@
-
-
-using Chirp.Razor;
+namespace Chirp.Razor;
 
 public interface ICheepService
 {
@@ -10,32 +8,32 @@ public interface ICheepService
 
 public class CheepService : ICheepService
 {
-    private ICheepRepository repository;
+    private ICheepRepository _repository;
 
 
     public CheepService(ICheepRepository repository)
     {
-        this.repository = repository;
+        this._repository = repository;
     }
     
-    public async Task<List<CheepDTO>> GetCheeps(int page) //TODO ASK TA IF THIS IS ILLEGAL
-       {
-           if ( page == 0 )
-           {
-               page = 1;
-           }
-           var queryresult = await repository.GetCheeps(page);
-           var result = new List<CheepDTO>();
-           foreach (var cheep in queryresult)
-           {    
-               var dto = new CheepDTO();
-               dto.author = cheep.Author.Name;
-               dto.message = cheep.Text;
-               dto.timestamp = cheep.Timestamp;
-               result.Add(dto);
-           }
-           return result;
-       }
+    public async Task<List<CheepDTO>> GetCheeps(int page)
+    {
+        if ( page == 0 )
+        {
+            page = 1;
+        }
+        var queryresult = await _repository.GetCheeps(page);
+        var result = new List<CheepDTO>();
+        foreach (var cheep in queryresult)
+        {    
+            var dto = new CheepDTO();
+            dto.author = cheep.Author.Name;
+            dto.message = cheep.Text;
+            dto.timestamp = cheep.Timestamp;
+            result.Add(dto);
+        }
+        return result;
+    }
 
     public async Task<List<CheepDTO>> GetCheepsFromAuthor(int page, string author)
     {
@@ -43,7 +41,7 @@ public class CheepService : ICheepService
         {
             page = 1;
         }
-        var queryresult = await repository.GetCheepsFromAuthor(page, author);
+        var queryresult = await _repository.GetCheepsFromAuthor(page, author);
         var result = new List<CheepDTO>();
         foreach (var cheep in queryresult)
         {    
