058bf73054da8b5113c7a8833f5d7618fdd05237
Mads Christensen <Mawc@itu.dk>
2024-10-09 13:40:39 +0200

    Fixed warnings in Cheep.cs

--- src/Chirp.Razor/Datamodel/Cheep.cs
+++ src/Chirp.Razor/Datamodel/Cheep.cs
@@ -1,13 +1,17 @@
-﻿using System.Runtime.CompilerServices;
+﻿
+using System.ComponentModel.DataAnnotations;
 
 namespace Chirp.Razor.Datamodel;
 
 public class Cheep
 {
     public int CheepId { get; set; }
-    public string Text { get; set; }
+    [StringLength(160)]
+    [Required]
+    public required string Text { get; set; }
     public DateTime Timestamp { get; set; }
-    public Author Author { get; set; }
+    [Required]
+    public required Author Author { get; set; }
     public int AuthorId { get; set; }
     
 }
