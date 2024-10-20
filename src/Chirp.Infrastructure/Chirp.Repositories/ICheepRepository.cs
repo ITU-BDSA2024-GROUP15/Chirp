d536ad7cef7b17a7d144ff4984b9e0f6e00b04e5
Marius Holm <marho@itu.dk>
2024-10-20 17:44:57 +0200

    Added functionality for adding cheeps to the database

--- src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
@@ -6,4 +6,8 @@ public interface ICheepRepository
 {
     public Task<List<Cheep>> GetCheeps(int page);
     public Task<List<Cheep>> GetCheepsFromAuthor(int page, string author); 
+    
+    public void AddCheepWithNoAuthor(string author, string text, string email);
+    
+    public void AddCheepWithAuthor(string author, string text);
 }
