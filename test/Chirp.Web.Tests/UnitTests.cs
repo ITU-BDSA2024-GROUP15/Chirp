1715d5feb21e175b5af88335fecbafec10556f2e
KassandraW <kwad@itu.dk>
2024-10-22 14:46:12 +0200

    refactored old tests to make use of in-memory Sqlite database
    Co-authored-by: Morten <mofb@itu.dk>
    Co-authored-by: Marius <marho@itu.dk>

--- test/Chirp.Web.Tests/UnitTests.cs
+++ test/Chirp.Web.Tests/UnitTests.cs
@@ -8,111 +8,106 @@ namespace Chirp.Web.Tests;
 
 public class UnitTests
 {
-
-    public UnitTests()
-    {
-    }
     
-    
-    //CheepService
+    //CheepRepository
     [Fact]
     public async Task TestGetCheepsAmount()
     {
-        var _repository = await TestUtilities.createInMemoryDB();
+        var repository = await TestUtilities.createInMemoryDB();
                 
-        var cheeps = await _repository.GetCheeps(0);
+        var cheeps = await repository.GetCheeps(0);
         Assert.Equal(32,cheeps.Count);
         TestUtilities.closeConnection();
         
     }
     
     
-    /*
+    
     [Fact]
     public async Task TestGetCheepsPage1()
     {
-        
-    }
-    
-    [Fact]
-    public void TestGetCheepsPage2()
-    {
-        ICheepService service = new CheepService();
-        service.ChangeDB(new DBFacade("test"));
-        List<Cheep> cheeps = service.GetCheeps(2);
+        var repository = await TestUtilities.createInMemoryDB();
+        var cheeps = await repository.GetCheeps(0);
         var cheep = cheeps[0];
-        Assert.Equal("In the morning of the wind, some few splintered planks, of what present avail to him.", cheep.Message);  
+        Assert.Equal("Starbuck now is what we hear the worst.", cheep.Text);
+        TestUtilities.closeConnection();
     }
     
     [Fact]
-    public void TestGetCheepsLastPage() 
+    public async Task TestGetCheepsPage2()
     {
-        ICheepService service = new CheepService();
-        service.ChangeDB(new DBFacade("test"));
-        List<Cheep> cheeps = service.GetCheeps(21);
+        
+        var repository = await TestUtilities.createInMemoryDB();
+        var cheeps = await repository.GetCheeps(2);
+        var cheep = cheeps[0];
+        Assert.Equal("In the morning of the wind, some few splintered planks, of what present avail to him.", cheep.Text);  
+        TestUtilities.closeConnection();
+    }
+    
+    
+    [Fact]
+    public async Task TestGetCheepsLastPage() 
+    {
+        var repository = await TestUtilities.createInMemoryDB();
+        var cheeps = await repository.GetCheeps(21);
         
         Assert.True(cheeps.Count == 17);
+        TestUtilities.closeConnection();
     }
     
     [Fact]
-    public void TestGetCheepsBeyondLimit() 
-    {
-        ICheepService service = new CheepService();
-        service.ChangeDB(new DBFacade("test"));
-        List<Cheep> cheeps = service.GetCheeps(32);
+    public async Task TestGetCheepsBeyondLimit() 
+    {   
+        var repository = await TestUtilities.createInMemoryDB();
+        var cheeps = await repository.GetCheeps(32);
         
         Assert.True(cheeps.Count == 0);    
+        TestUtilities.closeConnection();
     }
     
     
     [Fact]
-    public void TestGetCheepsFromAuthor() 
-    {
-        ICheepService service = new CheepService();
-        service.ChangeDB(new DBFacade("test"));
-        List<Cheep> cheeps = service.GetCheepsFromAuthor("Jacqualine Gilcoine", (0));
-        //we know Jacqualine isn't the first author on the public timeline.
-        Assert.True(cheeps[0].Author == "Jacqualine Gilcoine" && cheeps.Count == 32);
+    public async Task TestGetCheepsFromAuthor() 
+    {   
+        var repository = await TestUtilities.createInMemoryDB();
+        var cheeps = await repository.GetCheepsFromAuthor(0, "Jacqualine Gilcoine");
+        var cheep = cheeps[0];
+        //we know Jacqualine is the first author on the public timeline.
+        Assert.True(cheep.Author.Name == "Jacqualine Gilcoine"  && cheeps.Count == 32);
+        TestUtilities.closeConnection();
     }
     
     [Fact]
-    public void TestGetCheepsFromAuthorPage2()
+    public async Task TestGetCheepsFromAuthorPage2()
     {
-        ICheepService service = new CheepService();
-        service.ChangeDB(new DBFacade("test"));
-        List<Cheep> cheeps = service.GetCheepsFromAuthor("Jacqualine Gilcoine", (2));
+        var repository = await TestUtilities.createInMemoryDB();
+        var cheeps = await repository.GetCheepsFromAuthor(2, "Jacqualine Gilcoine");
+        var cheep = cheeps[0];
         
-        Assert.Equal("What a relief it was the place examined.", cheeps[0].Message);
+        Assert.Equal("What a relief it was the place examined.", cheep.Text);
+        TestUtilities.closeConnection();
     }    
     
     [Fact]
-    public void TestGetCheepsFromAuthorLastPage()
+    public async Task TestGetCheepsFromAuthorLastPage()
     {
-        ICheepService service = new CheepService();
-        service.ChangeDB(new DBFacade("test"));
-        List<Cheep> cheeps = service.GetCheepsFromAuthor("Jacqualine Gilcoine", (12));
+        var repository = await TestUtilities.createInMemoryDB();
+        var cheeps = await repository.GetCheepsFromAuthor(12, "Jacqualine Gilcoine");
+        var cheep = cheeps[0];
         
-        Assert.True(cheeps.Count == 7 && cheeps[0].Author == "Jacqualine Gilcoine");    
+        Assert.True(cheeps.Count == 7 && cheep.Author.Name == "Jacqualine Gilcoine"); 
+        TestUtilities.closeConnection();
     }
     
     [Fact]
-    public void TestGetCheepsFromAuthorBeyondLimit()
+    public async Task TestGetCheepsFromAuthorBeyondLimit()
     {
-        ICheepService service = new CheepService();
-        service.ChangeDB(new DBFacade("test"));
-        List<Cheep> cheeps = service.GetCheepsFromAuthor("Jacqualine Gilcoine", (20));
+        var repository = await TestUtilities.createInMemoryDB();
+        var cheeps = await repository.GetCheepsFromAuthor(20, "Jacqualine Gilcoine");
         
         Assert.True(cheeps.Count == 0);   
+        TestUtilities.closeConnection();
     }    
 
-
-
-    //DBFacade
-    [Fact]
-    public void TestParseDateTime()
-    {
-        Assert.Equal("23:21:56 07-09-2024", DBFacade.parseDateTime(1725744116));
-    }
-    */
     
 }
