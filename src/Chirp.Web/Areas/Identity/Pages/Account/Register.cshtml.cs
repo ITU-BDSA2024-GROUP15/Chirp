1a78d618d4cd432f1a40ff54cf7f46da03396b76
Morten <mofb@itu.dk>
2024-10-31 14:16:38 +0100

    Made it possible for logged in users to display there username, instead of email part1.
    Co-authored-by: Marius <marho@itu.dk>
    Co-authored-by: Kassandra <kwad@itu.dk>

--- src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -122,9 +122,10 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
                 
                 var user = CreateUser();
                 
-                user.UserName = Input.Name;
+                user.Name = Input.Name;
+                user.Email = Input.Email;
 
-                await _userStore.SetUserNameAsync(user, Input.Name, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
