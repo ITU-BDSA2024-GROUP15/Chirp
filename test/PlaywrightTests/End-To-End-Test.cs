67fce54313f0b736f738d7d3e84d0ee5e417ec27
iast <iast@itu.dk>
2024-11-08 15:06:02 +0100

    Playwright testing is working

---
+++ test/PlaywrightTests/End-To-End-Test.cs
@@ -0,0 +1,30 @@
+﻿namespace PlaywrightTests;
+
+
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class EndToEnd
+{
+    [Test]
+    public async Task HasTitle()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+        });
+        var context = await browser.NewContextAsync();
+
+        var page = await context.NewPageAsync();
+        await page.GotoAsync("https://bdsa2024group15chirprazor.azurewebsites.net/");
+        await page.Locator("p").Filter(new() { HasText = "Jacqualine Gilcoine Starbuck" }).GetByRole(AriaRole.Link).ClickAsync();
+        await page.GetByRole(AriaRole.Link, new() { Name = "Public Timeline" }).ClickAsync();
+        await page.Locator("body").ClickAsync();
+    }
+}
