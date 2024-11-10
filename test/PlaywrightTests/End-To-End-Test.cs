629eeb6aa6cd0a6aa0edc098b1146eb0dbee8529
Marius Holm <marho@itu.dk>
2024-11-10 21:37:03 +0100

    Added inheritance from PageTest and added another playwright test

--- test/PlaywrightTests/End-To-End-Test.cs
+++ test/PlaywrightTests/End-To-End-Test.cs
@@ -9,22 +9,29 @@ using NUnit.Framework;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
-public class EndToEnd
+public class EndToEnd : PageTest
 {
+    
     [Test]
     public async Task HasTitle()
     {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false,
-        });
-        var context = await browser.NewContextAsync();
-
-        var page = await context.NewPageAsync();
-        await page.GotoAsync("https://bdsa2024group15chirprazor.azurewebsites.net/");
-        await page.Locator("p").Filter(new() { HasText = "Jacqualine Gilcoine Starbuck" }).GetByRole(AriaRole.Link).ClickAsync();
-        await page.GetByRole(AriaRole.Link, new() { Name = "Public Timeline" }).ClickAsync();
-        await page.Locator("body").ClickAsync();
+        await Page.GotoAsync("https://bdsa2024group15chirprazor.azurewebsites.net/");
+        await Page.GotoAsync("https://bdsa2024group15chirprazor.azurewebsites.net/");
+        await Page.Locator("p").Filter(new() { HasText = "Jacqualine Gilcoine Starbuck" }).GetByRole(AriaRole.Link).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Public Timeline" }).ClickAsync();
+        await Page.Locator("body").ClickAsync();
     }
+
+    [Test]
+    public async Task CanClickRegister()
+    {
+        await Page.GotoAsync("https://bdsa2024group15chirprazor.azurewebsites.net/");
+        
+        
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Register" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Create a new account." })).ToBeVisibleAsync();
+    }
+    
+    
+    
 }
