f32177b8396bc91433a90d79f1085d31231ae6f0
Morten <mofb@itu.dk>
2024-11-13 14:53:40 +0100

    Username is now unique

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -5,14 +5,6 @@ namespace Chirp.Core;
 
 public class Author : IdentityUser<int>
 {
-    
-    //TODO Our identity now has two ID's. Gotta fix
-    /*
-    [StringLength(100)]
-    [Required]
-    public required int AuthorId { get; set; }
-    */
-    
     [StringLength(100)]
     [Required]
     public required string Name { get; set; }
@@ -20,7 +12,7 @@ public class Author : IdentityUser<int>
     [StringLength(100)]
     [Required]
     public new required string Email { get; set; } //TODO make email and name unique 
-    
+     
     public List<Cheep>? Cheeps { get; set; }
     
 }
