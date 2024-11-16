4147533e872baf706f9e3f825473d14142d84eb0
Marius Holm <marho@itu.dk>
2024-11-16 16:09:49 +0100

    Changed key to be a composite key of AuthorID and FollowsID meaning that a combi of two authors are unique

--- src/Chirp.Core/Follow.cs
+++ src/Chirp.Core/Follow.cs
@@ -1,12 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Core;
 
+[PrimaryKey(nameof(AuthorId), nameof(FollowsId))]
 public class Follow
 {
     [Required]
+    public int AuthorId { get; set; }
+    [Required]
     public required Author Author { get; set; }
     
+    [Required]
+    public int FollowsId { get; set; }
     [Required]
     public required Author Follows { get; set; }
 }
