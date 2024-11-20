971a59d13252378ffefebdb833ade464d9428c93
Mads Christensen <Mawc@itu.dk>
2024-11-20 15:22:10 +0100

    Changed Schema of Follow.cs to not include ids We decided they were not necessary and exposed userdata unnecessarily
    Co-authored-by: Marius <marho@itu.dk>

--- src/Chirp.Core/Follow.cs
+++ src/Chirp.Core/Follow.cs
@@ -3,17 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Core;
 
-[PrimaryKey(nameof(AuthorId), nameof(FollowsId), nameof(AuthorName))]
+[PrimaryKey(nameof(AuthorName),nameof(FollowsAuthorName))]
 public class Follow
 {
-    [Required]
-    public int AuthorId { get; set; }
+
     [Required]
     [StringLength(100)]
     public required string AuthorName { get; set; }
-    
-    [Required]
-    public int FollowsId { get; set; }
     [Required]
     [StringLength(100)]
     public required string FollowsAuthorName { get; set; }
