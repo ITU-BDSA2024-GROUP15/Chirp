0ed58c5ae89d043dedc2412ee095d7cd37712838
Marius Holm <mariusholm@hotmail.dk>
2024-11-28 12:53:16 +0100

    feat: created like schema

---
+++ src/Chirp.Core/Like.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirp.Core;
+
+[PrimaryKey(nameof(CheepId),nameof(AuthorName))]
+public class Like
+{
+    [Required]
+    public required int CheepId { get; set; } 
+    [Required]
+    public required string AuthorName { get; set; } 
+}
