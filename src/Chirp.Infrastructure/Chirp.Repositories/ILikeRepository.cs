be577c4adefa7a9fbbd6242cd3c545c2b0886cad
Marius Holm <mariusholm@hotmail.dk>
2024-11-28 12:53:48 +0100

    feat: created ILikeRepository.cs interface with two methods: AddLike & RemoveLike

---
+++ src/Chirp.Infrastructure/Chirp.Repositories/ILikeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Chirp.Core;
+
+namespace Chirp.Infrastructure.Chirp.Repositories;
+
+public interface ILikeRepository
+{
+    public Task AddLike(string authorName, Cheep cheep);
+    public Task RemoveLike(string authorName, Cheep cheep);
+}
