a56d8f2717ab30393e5ef842c84d44a6e170505a
Marius Holm <mariusholm@hotmail.dk>
2024-11-28 13:17:35 +0100

    fix: added documentation in Like class

--- src/Chirp.Core/Like.cs
+++ src/Chirp.Core/Like.cs
@@ -3,11 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Core;
 
+/// <summary>
+/// Represents a like in the Chirp application
+/// </summary>
 [PrimaryKey(nameof(CheepId),nameof(AuthorName))]
 public class Like
 {
     [Required]
     public required int CheepId { get; set; } 
+    
+    /// <summary>
+    /// Name of the author who liked the cheep
+    /// </summary>
     [Required]
-    public required string AuthorName { get; set; } 
+    public required string AuthorName { get; set; }
 }
