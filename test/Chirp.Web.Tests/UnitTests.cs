0bcccf04afb23346434bbac5515a424797adab71
KassandraW <kwad@itu.dk>
2024-12-06 14:35:50 +0100

    fix: warnings begone

--- test/Chirp.Web.Tests/UnitTests.cs
+++ test/Chirp.Web.Tests/UnitTests.cs
@@ -621,7 +621,7 @@ public class UnitTests : IAsyncLifetime
     [Fact]
     public async Task TestCreateDuplicateAuthorName()
     {
-        if (_authorRepository == null || _utils == null)
+        if (_authorRepository == null || _utils == null || _context == null)
         {
             return;
         }
@@ -641,7 +641,7 @@ public class UnitTests : IAsyncLifetime
     [Fact]
     public async Task TestCreateDuplicateAuthorEmail()
     {
-        if (_authorRepository == null || _utils == null)
+        if (_authorRepository == null || _utils == null || _context == null)
         {
             return;
         }
@@ -677,7 +677,7 @@ public class UnitTests : IAsyncLifetime
     
     
     
-    // ----  Followrepository ----
+    // ----  FollowRepository ----
     
     [Fact]
     public async Task CanAddFollowerToDb() 
