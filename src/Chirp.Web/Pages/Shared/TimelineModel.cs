c70a826b63a8cb0d8e7de8aefddbe138042b04ef
Mads Christensen <Mawc@itu.dk>
2024-12-06 13:57:39 +0100

    feat!: Created TimelineModel.cs Added common code and removed redundant code.
    Co-authored-by: Marius <marho@itu.dk>

---
+++ src/Chirp.Web/Pages/Shared/TimelineModel.cs
@@ -0,0 +1,105 @@
+﻿using Chirp.Core;
+using Chirp.Infrastructure.Chirp.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Build.Framework;
+
+namespace Chirp.Web.Pages.Shared;
+
+public class TimelineModel : PageModel
+{
+    private readonly IChirpService _service;
+    public int PageNumber { get; set; }
+    [BindProperty]
+    [Required]
+    public string? CheepMessage { get; set; }
+    
+    [BindProperty]
+    public string? FollowsName { get; set; }
+    
+    [BindProperty]
+    public int? LikedCheepId { get; set; }
+    
+    public List<CheepDto>? Cheeps { get; set; }
+    
+    public string? PageName { get; set; }
+    
+    public TimelineModel(IChirpService service)
+    {
+        _service = service;
+    }
+    
+    
+    public async Task<IActionResult> OnPost()
+    {
+        //We check if any validation rules has exceeded
+        if ( !ModelState.IsValid )
+        {
+            return Page();
+        }
+        
+        var authorName = User.Identity?.Name;
+        if ( authorName == null )
+        {
+            return Page();
+        }
+        var author = await _service.GetAuthorDtoByName(authorName);
+        if ( author == null )
+        {
+            return Page();
+        }
+
+        if (CheepMessage != null) await _service.AddCheep(CheepMessage, author.Username, author.Email);
+
+        return RedirectToPage(PageName);
+    }
+    
+    
+    public async Task<IActionResult> OnPostFollow()
+    {
+        var authorName = User.Identity?.Name;
+        
+        if (authorName != null)
+            if (FollowsName != null)
+                await _service.AddFollowing(authorName, FollowsName);
+
+        return RedirectToPage(PageName);
+    }
+    
+    public async Task<IActionResult> OnPostUnfollow()
+    {
+        var authorName = User.Identity?.Name;
+
+
+        if (authorName != null)
+            if (FollowsName != null)
+                await _service.RemoveFollowing(authorName, FollowsName);
+
+        return RedirectToPage(PageName);
+    }
+    
+    public async Task<IActionResult> OnPostLike()
+    {
+        var authorName = User.Identity?.Name;
+
+        if (authorName != null && LikedCheepId != null)
+        {
+            await _service.AddLike(authorName, LikedCheepId.Value);
+        }
+        
+        return RedirectToPage(PageName);
+    }
+    
+    public async Task<IActionResult> OnPostUnlike()
+    {
+        var authorName = User.Identity?.Name;
+
+        if (authorName != null && LikedCheepId != null)
+        {
+            await _service.RemoveLike(authorName, LikedCheepId.Value);
+        }
+        
+        return RedirectToPage(PageName);
+    }
+}
